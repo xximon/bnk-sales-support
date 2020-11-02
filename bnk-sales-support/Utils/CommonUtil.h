d00eeeeda42cc3b4e292015b588d062ea40da050
xximon <yr1633@gmail.com>
2020-11-02 17:29:23 +0900

    공통 유틸 클래스 생성

---
+++ bnk-sales-support/Utils/CommonUtil.h
@@ -0,0 +1,16 @@
+//
+//  CommonUtil.h
+//  bnk-sales-support
+//
+//  Created by YOUNGRAE SEO on 2020/11/02.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CommonUtil : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ bnk-sales-support/Utils/CommonUtil.m
@@ -0,0 +1,12 @@
+//
+//  CommonUtil.m
+//  bnk-sales-support
+//
+//  Created by YOUNGRAE SEO on 2020/11/02.
+//
+
+#import "CommonUtil.h"
+
+@implementation CommonUtil
+
+@end
