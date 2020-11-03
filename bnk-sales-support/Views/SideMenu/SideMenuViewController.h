3095c01be574d60a6673fb44293d9c3bcbee648e
xximon <yr1633@gmail.com>
2020-11-03 16:31:22 +0900

    사이드메뉴 뷰 컨트롤러 생성

---
+++ bnk-sales-support/Views/SideMenu/SideMenuViewController.h
@@ -0,0 +1,16 @@
+//
+//  SideMenuViewController.h
+//  bnk-sales-support
+//
+//  Created by YOUNGRAE SEO on 2020/11/03.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SideMenuViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ bnk-sales-support/Views/SideMenu/SideMenuViewController.m
@@ -0,0 +1,31 @@
+//
+//  SideMenuViewController.m
+//  bnk-sales-support
+//
+//  Created by YOUNGRAE SEO on 2020/11/03.
+//
+
+#import "SideMenuViewController.h"
+
+@interface SideMenuViewController ()
+
+@end
+
+@implementation SideMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
